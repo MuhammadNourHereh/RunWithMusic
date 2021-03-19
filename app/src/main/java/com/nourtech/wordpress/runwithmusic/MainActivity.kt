0596908596ecda88b030dbb10436026fcefb3531
MuhammadNourHereh <acadamiclearner@gmail.com>
2021-03-19 16:15:00 +0200

    first commit

---
+++ app/src/main/java/com/nourtech/wordpress/runwithmusic/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.nourtech.wordpress.runwithmusic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
