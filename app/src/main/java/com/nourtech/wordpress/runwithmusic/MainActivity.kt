9a1d0e496ca489e170df33555b8e61fd27aaebb6
MuhammadNourHereh <acadamiclearner@gmail.com>
2021-03-19 22:09:48 +0200

    setup data binding

--- app/src/main/java/com/nourtech/wordpress/runwithmusic/MainActivity.kt
+++ app/src/main/java/com/nourtech/wordpress/runwithmusic/MainActivity.kt
@@ -2,10 +2,17 @@ package com.nourtech.wordpress.runwithmusic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.nourtech.wordpress.runwithmusic.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // setup binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
